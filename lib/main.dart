520f582dc48f7bdf2f1bcaff511231489e6d91da
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2024-02-13 19:20:02 -0500

    Crear stateless widget para pagina temporal

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/home_temp.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Componentes'),
+        ),
+        body: const HomePageTemp(),
       ),
     );
   }
---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  const HomePageTemp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Home page patrick"),
+    );
+  }
+}
